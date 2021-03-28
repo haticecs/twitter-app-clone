3d3fc9b1e5d32cc8ceab63f051950c1fa46c9da6
haticecs <57989556+haticecs@users.noreply.github.com>
2021-03-28 15:21:26 +0300

    aws amplify configuration with authentication

--- App.tsx
+++ App.tsx
@@ -1,23 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React from 'react'
+import Amplify from 'aws-amplify'
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+// @ts-ignore
+import { withAuthenticator } from 'aws-amplify-react-native'
 
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+import useCachedResources from './hooks/useCachedResources'
+import useColorScheme from './hooks/useColorScheme'
+import Navigation from './navigation'
+
+// @ts-ignore
+import config from './src/aws-exports'
+
+Amplify.configure(config)
+
+function App() {
+  const isLoadingComplete = useCachedResources()
+  const colorScheme = useColorScheme()
 
   if (!isLoadingComplete) {
-    return null;
+    return null
   } else {
     return (
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
       </SafeAreaProvider>
-    );
+    )
   }
 }
+
+export default withAuthenticator(App)
