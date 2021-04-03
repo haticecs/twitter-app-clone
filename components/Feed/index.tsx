c97f4285831adecc746ecb90cd590d29ffeab273
haticecs <57989556+haticecs@users.noreply.github.com>
2021-04-03 13:34:39 +0300

    feed refreshing added

--- components/Feed/index.tsx
+++ components/Feed/index.tsx
@@ -6,18 +6,21 @@ import { listTweets } from '../../src/graphql/queries'
 
 const Feed = () => {
   const [tweets, setTweets] = useState([])
+  const [loading, setLoading] = useState(false)
 
-  useEffect(() => {
-    const fetchTweets = async () => {
-      //get the tweets from backend and set it to state
-      try {
-        const tweetsData = await API.graphql(graphqlOperation(listTweets))
-        //console.log(tweetsData.data.listTweets.items)
-        setTweets(tweetsData.data.listTweets.items)
-      } catch (error) {
-        console.log(error)
-      }
+  const fetchTweets = async () => {
+    //get the tweets from backend and set it to state
+    try {
+      setLoading(true)
+      const tweetsData = await API.graphql(graphqlOperation(listTweets))
+      setTweets(tweetsData.data.listTweets.items)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
     }
+  }
+  useEffect(() => {
     fetchTweets()
   }, [])
 
@@ -27,6 +30,8 @@ const Feed = () => {
         data={tweets}
         renderItem={({ item }) => <Tweet tweet={item} />}
         keyExtractor={(item) => item.id}
+        refreshing={loading}
+        onRefresh={fetchTweets}
       />
     </View>
   )
