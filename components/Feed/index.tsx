70477de216326701f07621212f17f244501acf27
haticecs <57989556+haticecs@users.noreply.github.com>
2021-04-03 13:20:09 +0300

    getting tweets from backend

--- components/Feed/index.tsx
+++ components/Feed/index.tsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, FlatList } from 'react-native'
-
-import tweets from '../../data/tweets'
-
+import { API, graphqlOperation } from 'aws-amplify'
 import Tweet from '../Tweet'
+import { listTweets } from '../../src/graphql/queries'
 
-const Feed = () => (
-  <View style={{ width: '100%' }}>
-    <FlatList
-      data={tweets}
-      renderItem={({ item }) => <Tweet tweet={item} />}
-      keyExtractor={(item) => item.id}
-    />
-  </View>
-)
+const Feed = () => {
+  const [tweets, setTweets] = useState([])
+
+  useEffect(() => {
+    const fetchTweets = async () => {
+      //get the tweets from backend and set it to state
+      try {
+        const tweetsData = await API.graphql(graphqlOperation(listTweets))
+        //console.log(tweetsData.data.listTweets.items)
+        setTweets(tweetsData.data.listTweets.items)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchTweets()
+  }, [])
+
+  return (
+    <View style={{ width: '100%' }}>
+      <FlatList
+        data={tweets}
+        renderItem={({ item }) => <Tweet tweet={item} />}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  )
+}
 
 export default Feed
